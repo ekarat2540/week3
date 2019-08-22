9bfd334da4aae75fca366f40e3e632fb1995635e
Ekarat Sukprasert <59160993@go.buu.ac.th>
2019-08-22 20:34:11 +0700

    ImageView added

--- app/src/main/java/com/example/work3/MainActivity.kt
+++ app/src/main/java/com/example/work3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.work3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -16,10 +17,10 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
-        var resetButton:Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener{resetButton()}
+        //var resetButton:Button = findViewById(R.id.reset_button)
+        //resetButton.setOnClickListener{resetButton()}
 
-        val resultText: TextView  = findViewById(R.id.result_text)
+
 
         val setButton: Button = findViewById(R.id.reset_button)
 
@@ -27,14 +28,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice(){
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView  = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        //resultText.text = randomInt.toString()
         Toast.makeText(this,"button clicked",
             Toast.LENGTH_SHORT).show()
     }
-    private fun resetButton(){
-        val resultText: TextView  = findViewById(R.id.result_text)
-        resultText.text = "0"
+   // private fun resetButton(){
+        //val resultText: TextView  = findViewById(R.id.result_text)
+       // resultText.text = "0"
 
-    }
+   // }
 }
